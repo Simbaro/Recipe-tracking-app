7af8fede51786bb836e0320092eff3a9a0c60d13
Adrian Nuño <nuno29a@gmail.com>
2021-12-17 12:26:19 -0600

    Updates

---
+++ precache-manifest.50452fe959a1e318df5c0c122af0235d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5452ce4d1f8013140d0d3bf70b12604c",
+    "url": "/Recipe-tracking-app/index.html"
+  },
+  {
+    "revision": "130b5afaf7767be80d64",
+    "url": "/Recipe-tracking-app/static/css/main.32b4a685.chunk.css"
+  },
+  {
+    "revision": "f394498dd0083a989c2b",
+    "url": "/Recipe-tracking-app/static/js/2.6d741cec.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Recipe-tracking-app/static/js/2.6d741cec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "130b5afaf7767be80d64",
+    "url": "/Recipe-tracking-app/static/js/main.870ec47a.chunk.js"
+  },
+  {
+    "revision": "fbdb3390a1871d8df552",
+    "url": "/Recipe-tracking-app/static/js/runtime-main.19612c3d.js"
+  }
+]);

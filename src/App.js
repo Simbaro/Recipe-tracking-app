75a0b3607565149f9df18261cb820bd8a3cf9a1c
Adrian Nuño <nuno29a@gmail.com>
2021-12-17 12:18:33 -0600

    Deploy React Project live to GitHub Pages

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import RecipeCreate from "./RecipeCreate";
+import RecipeList from "./RecipeList";
+import RecipeData from "./RecipeData";
+import "./App.css";
+
+// TODO: Add the ability for the <RecipeList /> component to list, edit and delete an existing recipe.
+  // TODO: Add the ability for the <RecipeCreate /> component to create new recipes.
+
+function App() {
+  //Create recipe state variables
+  const [recipes, setRecipes] = useState(RecipeData);
+
+  //Two functions to add and delete recipes
+  const addRecipe = (newRecipe) => {
+    setRecipes([ ...recipes, newRecipe ]);
+  };
+
+  function deleteRecipe(recipeIndex) {
+    setRecipes(recipes.filter((recipe, index) => index !== recipeIndex));
+  };
+
+  //Return the rendered page using components
+  return (
+    <div className="App">
+      <header><h1>Delicious Food Recipes</h1></header>
+      <RecipeList recipes={recipes} deleteRecipe={deleteRecipe}/>
+      <RecipeCreate addRecipe={addRecipe} />
+    </div>
+  );
+}
+
+export default App;
